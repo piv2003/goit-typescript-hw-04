4763cfd24e4579733767cbc1c8b619d9e024ff8b
Ihor Poliakov <piv2003@gmail.com>
2023-11-29 00:09:24 +0200

    Update 1.tsx

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
